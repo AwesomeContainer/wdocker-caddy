51ce06abb5d7201019842486f56daf31d6b858d1
Awesome Container <AwesomeContainer@gmail.com>
2018-10-07 22:00:37 +0200

    Bugfix bootstrap

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -8,13 +8,13 @@ function Confirm-Directory {
  param( [string]$InputDir )
  if ((Test-Path -Path $InputDir) -eq $false)
  {
-    Write-Ouput "[$(Get-Date)]: The dir $InputDir does not exist. Creating it.. "
+    Write-Output "[$(Get-Date)]: The dir $InputDir does not exist. Creating it.. "
     New-Item -ItemType Directory -Path $InputDir 
  }
 }
 
 # Check if the config dirs are created, be sure to use a volume mount for this data.
-Write-Output "Checking config dirs in volume mount.. "
+Write-Output "[$(Get-Date)]: Checking config dirs in volume mount.. "
 Confirm-Directory "G:\conf"
 Confirm-Directory "G:\html"
 Confirm-Directory "G:\logs"
